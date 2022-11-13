c11031bd9c488b0fb97e0f184e2c096bdff171b6
yuto ichinose <yuto.0526.sano@gmail.com>
2022-11-14 07:21:46 +0900

    jsを外部ファイルから読み込ませるようにしたがsocket-clientはimportできなかった

---
+++ public/js/main.js
@@ -0,0 +1,44 @@
+"use strict";
+/// <reference path="https://cdn.socket.io/4.5.3/socket.io.min.js"/>
+
+//-------------------------------------
+// Socket.ioサーバへ接続
+//-------------------------------------
+const socket = io();
+
+/**
+ * [イベント] ページの読込み完了
+ */
+window.onload = () => {
+  /**
+   * [イベント] フォームが送信された
+   */
+  document.querySelector("#frm-post").addEventListener("submit", (e) => {
+    // 規定の送信処理をキャンセル(画面遷移しないなど)
+    e.preventDefault();
+
+    // 入力内容を取得する
+    const msg = document.querySelector("#msg");
+    if (msg.value === "") {
+      return false;
+    }
+
+    // Socket.ioサーバへ送信
+    socket.emit("post", { text: msg.value });
+
+    // 発言フォームを空にする
+    msg.value = "";
+  });
+
+  /**
+   * [イベント] 誰かが発言した
+   */
+  socket.on("member-post", (msg) => {
+    const list = document.querySelector("#msglist");
+    const li = document.createElement("li");
+    li.innerHTML = `${msg.text}`;
+    list.insertBefore(li, list.firstChild);
+  });
+  // テキストボックスを選択する
+  document.querySelector("#msg").focus();
+};
--- server.js
+++ server.js
@@ -1,4 +1,6 @@
-const app  = require("express")();
+const Express = require('express');
+const path = require('path');
+const app  = Express();
 const http = require("http").createServer(app);
 const io   = require("socket.io")(http);
 const port = 8081
@@ -9,6 +11,8 @@ const port = 8081
 app.get("/", (req, res)=>{
   res.sendFile(__dirname + "/index.html");
 });
+// /jsからjsファイルを出力
+app.use("/js", Express.static(path.join(__dirname, "/public/js")));
 
 /**
  * [イベント] ユーザーが接続
